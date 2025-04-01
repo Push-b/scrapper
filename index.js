2f7f54130cd6427447010b2211967728a1c5b1af
Deryl <153626742+Push-b@users.noreply.github.com>
2025-04-01 20:05:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const puppeteer = require("puppeteer-extra");
+const puppeteer = require("puppeteer");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 dotenv.config();
