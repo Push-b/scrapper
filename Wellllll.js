3204a5a5c98e4af28c01f63e4cd735bf21c63fc0
Deryl <153626742+Push-b@users.noreply.github.com>
2025-04-02 23:30:18 +0200

    Create Wellllll.js

---
+++ Wellllll.js
@@ -0,0 +1,13 @@
+http://localhost:5000/download?url=https://www.youtube.com/watch?v=VIDEO_ID&format=mp3
+
+
+
+
+
+
+
+
+
+
+
+http://localhost:5000/download?url=https://www.youtube.com/watch?v=VIDEO_ID&format=mp4
